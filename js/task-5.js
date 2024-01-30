1fe369fa6d31f4ce7f130572193973842ce89a2d
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-01-30 20:37:48 +0200

    finish

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,17 @@
+'use strict';
+
+const btnEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  const changeClr = (bodyEl.style.backgroundColor =
+    getRandomHexColor());
+  spanEl.textContent = changeClr;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,42 @@
+'use strict';
+
+const divBoxesEl = document.querySelector('#boxes');
+const inputEl = document.querySelector('input');
+const createBtnEl = document.querySelector(
+  'button[data-create]'
+);
+const destroyBtnEl = document.querySelector(
+  'button[data-destroy]'
+);
+
+createBtnEl.classList.add('create');
+destroyBtnEl.classList.add('destroy');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+function createBoxes(amount) {
+  destroyHandler();
+  amount = inputEl.value;
+  inputEl.value = '';
+  let boxSize = 30;
+  if (amount >= 1 && amount <= 100) {
+    for (let i = 0; i < amount; i++) {
+      const div = document.createElement('div');
+      div.style.width = `${boxSize}px`;
+      div.style.height = `${boxSize}px`;
+      div.style.backgroundColor = getRandomHexColor();
+      divBoxesEl.append(div);
+      boxSize += 10;
+    }
+  }
+}
+
+function destroyHandler() {
+  divBoxesEl.innerHTML = '';
+}
+
+createBtnEl.addEventListener('click', createBoxes);
+destroyBtnEl.addEventListener('click', destroyHandler);
